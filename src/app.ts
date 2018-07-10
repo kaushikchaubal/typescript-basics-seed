fdac922535860fc7af4769df7f31383a5258683d
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-10 10:49:31 +0100

    Adding optional params

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 
-let sumOrder: (price: number, quantity: number) => number;
+let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => x * y;
+sumOrder = (x, y) => {
+    if (y) {
+        return x * y;
+    } else {
+        return x;
+    }
+} 
 
-const sum = sumOrder(25, 2);
-
+const sum = sumOrder(25);
 console.log(`Total sum: ${sum}`);
+
+const secondSum = sumOrder(25, 2);
+console.log(`Total secondSum: ${secondSum}`);
