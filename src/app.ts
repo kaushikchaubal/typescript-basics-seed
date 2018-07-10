04053df65a57c1a67c5c7d1cd52e451e56349f43
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-10 10:51:46 +0100

    Adding optional params (in function def)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
 
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => {
-    if (y) {
-        return x * y;
-    } else {
-        return x;
-    }
-} 
-
+sumOrder = (x, y = 1) =>  x * y;
+   
 const sum = sumOrder(25);
 console.log(`Total sum: ${sum}`);
 
