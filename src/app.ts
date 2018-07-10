4699cf74e326700b0eb334c967de92e8ddc1051d
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-10 09:30:41 +0100

    Using arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
 console.log('Ssup? Kaushik!');
+
+const pizzas = [
+    {name: 'Pepporini', toppings: ['pepperoni']}
+];
+console.log(pizzas);
+
+const upperCasePizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+console.log(upperCasePizzas);
