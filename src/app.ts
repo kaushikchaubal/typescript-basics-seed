b02811a619b7a21b8aae3853c0a897462cced3b9
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-10 11:38:12 +0100

    Addig classes in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-type Pizza = { name: string; toppings: number };
-
-const pizza: Pizza = { name: 'KC Pizza', toppings: 5 };
-
-const serialized = JSON.stringify(pizza);
-
-function getNameFromJSON(obj: string) {
-  return (JSON.parse(obj) as Pizza).name;
-}
-
-getNameFromJSON(serialized);
+class Pizza {
+    name: string;
+    toppings: string[] = [];
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  
+    addTopping(topping: string) {
+      this.toppings.push(topping);
+    }
+  }
+  
+  const pizza = new Pizza('Pepperoni');
+  
+  pizza.addTopping('pepperoni');
+  
+  console.log(pizza);
