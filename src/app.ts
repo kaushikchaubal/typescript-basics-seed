6d7a2fb9b51a2485ea5bc572435efaacac92bd63
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-10 10:55:34 +0100

    Explicitly defining objects

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
+let pizza: {name: string, quantity: number}
 
-let sumOrder: (price: number, quantity?: number) => number;
+pizza = {
+    name: 'Kaushik\'s Pizza',
+    quantity: 5
+}
 
-sumOrder = (x, y = 1) =>  x * y;
-   
-const sum = sumOrder(25);
-console.log(`Total sum: ${sum}`);
-
-const secondSum = sumOrder(25, 2);
-console.log(`Total secondSum: ${secondSum}`);
+console.log(pizza);
